4db9060d753fc789bd15a3422d4014101ad03eb4
Nissaq <2020470222@student.uitm.edu.my>
2022-05-18 23:02:32 +0800

    assocArray.php

--- assocArray.php
+++ assocArray.php
@@ -11,7 +11,7 @@
 
 $month = array ('January','February','March','April','May','June','July','August','September','October','November','December');
 $daymonth = array ('January' => 31,'February' => 28,'March' => 31,'April' => 30,'May' => 31,'June' => 30,'July' => 31,'August' => 31,'September'=> 30,'October' => 31,'November' => 30,'December' => 31);
-;$l = 0;
+;$i = 0;
 
 foreach($daymonth as $val)
 {
